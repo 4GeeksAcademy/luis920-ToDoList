a064b86a9c6260836c48f171cae58a75820eccf2
Luis Galvan <67127384+luis920@users.noreply.github.com>
2024-10-30 13:44:32 +0000

    commit final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import {Todolist} from "./todolist.jsx";
 
 
 
 //create your first component
 const Home = () => {
+  
 	return (
 		<div >
 			<Todolist/>
--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
- export const Todolist = ()=>{
-    return(
+export const Todolist = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = () => {
+    if (inputValue.trim() !== "") {
+      setTareas([...tareas, inputValue]);
+      setInputValue(""); 
+    }
+  };
+
+  const eliminarTarea = (index) => {
+    setTareas(tareas.filter((_, currentIndex) => currentIndex !== index));
+  };
+
+  return (
     <>
       <div className="container">
-         <h1>Todos</h1>
-         <ul>
-           <li className="li-input"><input type="text" placeholder="Agregar tarea" /></li>
-           <li>hacer de comer<i class="fa-solid fa-trash"></i></li>
-           <li>ir a caminar<i class="fa-solid fa-trash"></i> </li>
-           <li>Estudiar<i class="fa-solid fa-trash"></i></li>
-           <li>Trabajar<i class="fa-solid fa-trash"></i></li>
-         </ul>
-         <div>23 tareas</div>
-
-
+        <h1>Todos</h1>
+        <ul>
+          <li className="li-input">
+            <input 
+              type="text" 
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  agregarTarea();
+                }
+              }}
+              placeholder="Agregar tarea" 
+            />
+            
+            </li>
+          {tareas.map((tarea, index) => (
+            <li key={index}>
+              {tarea}
+            <span>
+              <i 
+                className="icon fa-solid fa-trash" 
+                onClick={() => eliminarTarea(index)}
+              ></i>
+            </span>
+              
+            </li>
+          ))}
+        </ul>
+        <div>{tareas.length} tareas</div>
       </div>
-   
     </>
-
-    )
-   
-}
-
+  );
+};
